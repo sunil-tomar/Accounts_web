8c8ffc183c628b69e4ca83a2fe82e2b128a41dfc
suniltomar <suniltomar.developer@gmail.com>
2023-06-08 22:54:36 +0530

    code refactor : added relative path for h2 database file.
    code refactor : added relative path for h2 database file.

--- src/main/java/com/homemaker/Accounts/AccountsApplication.java
+++ src/main/java/com/homemaker/Accounts/AccountsApplication.java
@@ -1,7 +1,5 @@
 package com.homemaker.Accounts;
 
-import com.homemaker.Accounts.service.MonthlyExpenseServiceImpl;
-import com.homemaker.Accounts.serviceinterface.IMonthlyExpenseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
